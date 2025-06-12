e3b0455c8e958b8f6ff2cd71e2e921c8d54cdc15
lisiyuan <3215632117@qq.com>
2025-06-12 08:31:10 +0800

    glsc2

---
+++ order/src/test/java/com/sia/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sia.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
