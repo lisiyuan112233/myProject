69bc0f9e0185501799e99dacdb91c8171f9be9df
lisiyuan <3215632117@qq.com>
2025-06-12 09:00:54 +0800

    忽略垃圾文件

--- ware/src/main/java/com/sia/ware/WareApplication.java
+++ ware/src/main/java/com/sia/ware/WareApplication.java
@@ -1,5 +1,6 @@
 package com.sia.ware;
 
+import com.sia.ware.beans.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class WareApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WareApplication.class, args);
+        User user = new User("张三");
+        System.out.println(user);
     }
 
 }
---
+++ ware/src/main/java/com/sia/ware/beans/User.java
@@ -0,0 +1,12 @@
+package com.sia.ware.beans;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@ToString
+public class User {
+    private String name;
+}
